c0b8ac86ae27b414584d3acc7b4196d93d483bd4
Mainak Mondal <mainakmondal24@gmail.com>
2025-08-25 13:56:32 +0530

    nvim update from nvim-old

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,6 +17,8 @@ vim.keymap.set("n", "]", ":bnext<CR>")
 vim.keymap.set("n", "<Esc>", ":noh<CR>")
 vim.keymap.set("n", "-", ":Oil<CR>")
 vim.keymap.set("v", "q", "c\"<Esc>pa\"<Esc>", {desc = "surround selected code with quotes \" "} ) 
+vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>")
+vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
 
 --plugins
 vim.pack.add({
@@ -25,12 +27,17 @@ vim.pack.add({
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/mason-org/mason.nvim",
 	"https://github.com/stevearc/oil.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons"
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/ibhagwan/fzf-lua"
 })
 
 --setups
 require "mason".setup()
-require ("oil").setup()
+require 'oil'.setup({
+	view_options = {
+		show_hidden = true,
+	}
+})
 
 --lsp
 vim.lsp.enable({"gopls" , "lua_ls"})
