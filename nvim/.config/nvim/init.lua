1bdb304ca709a89de92e81d66ce900670814e973
Mainak Mondal <mainakmondal24@gmail.com>
2025-09-21 14:33:45 +0530

    Treesitter config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,14 +4,13 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.winborder = "rounded"
-vim.opt.completeopt = { "menu", "popup", "noselect" }
-vim.opt.autocomplete = true
-vim.opt.shiftwidth = 2
+vim.opt.completeopt = { "menuone", "popup", "noinsert" }
+vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.scrolloff = 8
-
+--vim.opt.showtabline = 2
 
 --keymaps
 vim.g.mapleader = " "
@@ -32,36 +31,50 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
 
-
 --plugins
 vim.pack.add({
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/catppuccin/nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/mason-org/mason.nvim",
-	"https://github.com/mason-org/mason-lspconfig.nvim",
-	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
-	"https://github.com/stevearc/oil.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons",
-	"https://github.com/ibhagwan/fzf-lua",
-	"https://github.com/Saghen/blink.cmp"
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/catppuccin/nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/mason-org/mason.nvim",
+    "https://github.com/mason-org/mason-lspconfig.nvim",
+    "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
+    "https://github.com/stevearc/oil.nvim",
+    "https://github.com/nvim-tree/nvim-web-devicons",
+    "https://github.com/ibhagwan/fzf-lua",
+    "https://github.com/Saghen/blink.cmp"
 })
 
 --color
 vim.cmd("colorscheme catppuccin-mocha")
 
 --setups
-require 'oil'.setup({ view_options = { show_hidden = true } })
+require "oil".setup({ view_options = { show_hidden = true } })
 require "mason".setup()
 require "mason-lspconfig".setup()
 require "blink.cmp".setup({
-  fuzzy = { implementation = "lua" },
-  keymap = { preset = "enter" },
-  completion = { list = { selection = { preselect = true } } }
+    fuzzy = { implementation = "lua" },
+    keymap = { preset = "enter" },
+    completion = { list = { selection = { preselect = true, auto_insert = false } }, trigger = { show_on_insert = false } }
 })
-
+require "nvim-treesitter.configs".setup({
+    ensure_installed = {"c", "cpp", "html", "javascript"},
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+	enable = true,
+	keymaps = {
+	    init_selection = "<leader>ss",
+	    node_incremental = "<leader>si",
+	    scope_incremental = "<leader>sc",
+	    node_decremental = "<leader>sd",
+	},
+    },
+})
 
 --lsp
 require "mason-tool-installer".setup({
-	ensure_installed = { "lua_ls", "html", "cssls" }
+    ensure_installed = { "lua_ls", "html", "cssls" }
 })
+
