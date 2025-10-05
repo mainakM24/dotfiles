9f89646184148a9538c0aee2419a4a58fcd0d8a1
Mainak Mondal <mainakmondal24@gmail.com>
2025-10-05 20:22:35 +0530

    Mini Indent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,23 +1,25 @@
+local vim = vim
 --options
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.winborder = "rounded"
-vim.opt.completeopt = { "menuone", "popup", "noinsert" }
+vim.opt.completeopt = { "menu", "noselect" }
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.scrolloff = 8
---vim.opt.showtabline = 2
+vim.g.netrw_banner = 0
+vim.g.indent_guide = false
 
 --keymaps
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", ":write<CR>")
 vim.keymap.set("n", "<leader>q", ":quit<CR>")
 vim.keymap.set("n", "<leader>wq", ":wq<CR>")
-vim.keymap.set("n", "<leader>e", ":Ex<CR>")
+vim.keymap.set("n", "<leader>e", ":25Lex<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
@@ -29,12 +31,15 @@ vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.format)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("n", "<C-h>", "<C-w>h", {desc = "Focus Left window"})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {desc = "Focus Right window"})
+
 
 
 --plugins
 vim.pack.add({
+    "https://github.com/vague2k/vague.nvim",
     "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/catppuccin/nvim",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/mason-org/mason.nvim",
     "https://github.com/mason-org/mason-lspconfig.nvim",
@@ -42,20 +47,26 @@ vim.pack.add({
     "https://github.com/stevearc/oil.nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/ibhagwan/fzf-lua",
-    "https://github.com/Saghen/blink.cmp"
+    "https://github.com/Saghen/blink.cmp",
+    "https://github.com/nvim-mini/mini.indentscope",
+    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
 
 --color
-vim.cmd("colorscheme catppuccin-mocha")
+vim.cmd.colorscheme("vague")
 
 --setups
-require "oil".setup({ view_options = { show_hidden = true } })
 require "mason".setup()
 require "mason-lspconfig".setup()
+require "mini.indentscope".setup({ draw = {delay = 0}, symbol = "│" })
+require 'oil'.setup({
+    default_file_explorer = false,
+    view_options = { show_hidden = true }
+})
 require "blink.cmp".setup({
     fuzzy = { implementation = "lua" },
     keymap = { preset = "enter" },
-    completion = { list = { selection = { preselect = true, auto_insert = false } }, trigger = { show_on_insert = false } }
+    completion = { list = { selection = { preselect = true } } }
 })
 require "nvim-treesitter.configs".setup({
     ensure_installed = {"c", "cpp", "html", "javascript"},
@@ -73,8 +84,8 @@ require "nvim-treesitter.configs".setup({
     },
 })
 
+
 --lsp
 require "mason-tool-installer".setup({
-    ensure_installed = { "lua_ls", "html", "cssls" }
+    ensure_installed = { "lua_ls", "html", "cssls", "clangd" }
 })
-
